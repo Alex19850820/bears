2c4e5ad14737cdd9e592818c396212d8cd746903
Александр <shlykovn@mail.ru>
2019-02-15 17:52:34 +0300

    mail

--- wp-content/themes/bears/includes/custom-functions.php
+++ wp-content/themes/bears/includes/custom-functions.php
@@ -6,7 +6,7 @@ add_action( 'wp_ajax_nopriv_sendForm', 'sendForm' );
 function sendForm() {
 	if (isset($_POST )) {
 		// обрабатываем запрос
-		$adminEmail = get_option('admin_email');
+		$adminEmail = get_option('admin_email').',Muravev.es@yandex.ru';
 //		$adminEmail = '89261231522@mail.ru,
 //globus_estate@mail.ru';
 		$name = ($_POST['name'] == 'undefined') ? '' : $_POST['name'] ?? '' ;
